915ca8a5de1dbc7efd1ccbd1e815fb8017ea443b
Odalmi <odpaulino2532@gmail.com>
2020-11-29 20:30:27 -0400

    Connection to Database

---
+++ src/database/database.js
@@ -0,0 +1,19 @@
+import Sequelize from 'sequelize';
+
+export const  sequelize = await new Sequelize(
+    'LavApp',
+    'DearPage25',
+    'Odalmi16',
+    {
+        host: 'mitechy.com',
+        dialect: 'postgres',
+        port: 5533,
+        pool:{
+            max: 5,
+            min: 0,
+            require: 30000,
+            idle: 10000,
+        },
+        logging: false
+    }
+);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import app from './server';
 import '@babel/polyfill';
-
+import Sequelize from 'sequelize';
 async function main() {
     await app.listen(300);
     console.log('server on port 3000');
+
 }
 
 main();
