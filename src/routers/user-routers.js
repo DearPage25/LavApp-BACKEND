8e64a6566685fb58dd6563c503c31cc078bb9577
Odalmi <odpaulino2532@gmail.com>
2021-05-02 13:13:38 -0400

    Cambios en user-routers

--- src/routers/user-routers.js
+++ src/routers/user-routers.js
@@ -4,7 +4,7 @@ import {verificaToken, verificaRole} from '../middlewares/authentications.js';
 import { createUser, getOneUser, getAllUser, updateUser } from '../controllers/usuario-controller';
 import { Login } from '../controllers/login-controller';
 router.post('/usuario',verificaRole, createUser);
-router.get('/usuario',verificaToken, getAllUser);
+router.get('/usuario',verificaRole, getAllUser);
 router.get('/usuario/:id_user',[verificaToken, verificaRole], getOneUser);
 router.put('/usuario/:id_user',[verificaToken, verificaRole], updateUser);
 router.post('/usuario/login', Login);
