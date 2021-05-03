2aa9e47a6c29f7d1e9c42e1f0f3263f8de14a879
Odalmi <odpaulino2532@gmail.com>
2021-05-02 20:35:55 -0400

    Quite los tokens para poder unir la app tranquilamente

--- src/routers/clothe-type-routers.js
+++ src/routers/clothe-type-routers.js
@@ -2,11 +2,11 @@ import { Router } from 'express';
 const router = Router();
 import { verificaToken } from '../middlewares/authentications';
 import { createClothe, getAllClothe, getOneClothe, deleteClothe, updateClothe } from '../controllers/clothe-type-controller'
-router.post('/clothe',verificaToken , createClothe);
-router.get('/clothe',verificaToken , getAllClothe);
-router.get('/clothe/:id_clothe_type',verificaToken , getOneClothe);
-router.put('/clothe/:id_clothe_type',verificaToken , updateClothe);
-router.delete('/clothe/:id_clothe_type',verificaToken , deleteClothe);
+router.post('/clothe', createClothe);
+router.get('/clothe', getAllClothe);
+router.get('/clothe/:id_clothe_type', getOneClothe);
+router.put('/clothe/:id_clothe_type', updateClothe);
+router.delete('/clothe/:id_clothe_type', deleteClothe);
 
 
 module.exports = router;
