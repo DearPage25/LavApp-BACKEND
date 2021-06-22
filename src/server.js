cb8c17181c82de6590e7fc6f45b0b6d0c3410111
Odalmi R. Paulino P <odalmipaulino@Odalmis-MacBook-Pro.local>
2021-06-22 19:44:17 -0400

    se agrego los cors a las dependencias

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
 require('../config.env');
 import express, {json} from 'express';
+import cors from 'cors';
 const app = express();
 
 import morgan from 'morgan';
 import router from './routers/index-routers';
 
-
 app.use(morgan('dev'));
 app.use(json());
 
+app.use(cors());
 //rutas
 app.use('/api', router);
 
